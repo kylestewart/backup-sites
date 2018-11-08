68e784c4c6cab5df3ac7936c9a0884bbbec7a66f
Kyle Stewart <git@kylestewart.com>
2018-11-08 23:33:12 +0000

    big changes today, finished the wordpress db dump portion.  S3 next.

--- backup-sites.py
+++ backup-sites.py
@@ -1,5 +1,9 @@
-import os,shutil,datetime,json
-from pprint import pprint
+import os
+import shutil
+import datetime
+import json
+import re
+import subprocess
 
 #CONSTANTS
 script_path = os.path.dirname(os.path.realpath(__file__))
@@ -21,11 +25,9 @@ def create_archive_dir(dstPath):
     archive = os.path.join(dstPath,date)
     try:
         os.makedirs(archive)
-    except FileExistsError:
+    except:
         archive = str(archive) + "." + str(datetime.datetime.now().strftime('%f')) 
         os.makedirs(archive)
-    except:
-        print("Failed to make destination directory")
     return(archive)
 
 #Prune old backup directories, as defined in the settings file
@@ -45,6 +47,16 @@ def prune_archives(dstPath,MaxArchives):
             shutil.rmtree(archives[i])
             i += 1
 
+#Search source directories for WordPress Config files
+def identify_wp_installs(srcPath):
+    print("{} {} {}".format("Scanning",srcPath,"for WordPress configs"))
+    wpInstalls = []
+    for dir in os.listdir(srcPath):
+        for file in os.listdir(os.path.join(srcPath,dir)):
+            if file == "wp-config.php":
+                wpInstalls.append(dir)
+    return(wpInstalls)
+
 #Backup content of source directories
 def archive_files(srcPath):
     archives = []
@@ -56,19 +68,48 @@ def archive_files(srcPath):
             shutil.make_archive(os.path.join(zipdir,dir),'zip',zipdir,os.path.join(srcPath,dir))
         else:
             shutil.copytree(os.path.join(srcPath,dir),os.path.join(dstPath,dir))
+    if backup_wp_dbs:
+        wp_installs = identify_wp_installs(srcPath)
+        if len(wp_installs) > 0:
+            backup_wp_databases(srcPath,dstPath,wp_installs)
+        else:
+            print("No WP Installs were found")
 
-        
-#Search source directories for WordPress Config files
-def identify_wp_installs(srcPath):
-    print("{} {} {}".format("Scanning",srcPath,"for WordPress configs"))
-    wpInstalls = []
-    for dir in os.listdir(srcPath):
-        for file in os.listdir(os.path.join(srcPath,dir)):
-            if file == "wp-config.php":
-                wpInstalls.append(dir)
-    print(wpInstalls)
- 
+#Take a list of WP Installs, snag the DB credentials, and take the dumps
+#TODO decide what happens when any of these ifs are false
+def backup_wp_databases(srcPath,dstPath,wpInstalls):
+    for install in wpInstalls:
+        dbinfo = {}
+        srcdir = os.path.join(srcPath,install)
+        dstdir = os.path.join(dstPath,install)
+        myconf = os.path.join(dstdir,"mylogin.cnf")
+        target = os.path.join(dstdir,install) + ".sql"
+        with open(os.path.join(srcdir,'wp-config.php')) as config_file:
+            for line in config_file:
+                name = re.search('define\(\'DB_NAME\'\,\ ?\'(?P<dbname>.*)\'\);',line)
+                if name:
+                    dbinfo['dbname'] = name.group('dbname')
+                    #print(dbinfo['dbname'])
+                user = re.search('define\(\'DB_USER\'\,\ ?\'(?P<dbuser>.*)\'\);',line)
+                if user:
+                    dbinfo['dbuser'] = user.group('dbuser')
+                    #print(dbinfo['dbuser'])
+                password = re.search('define\(\'DB_PASSWORD\'\,\ ?\'(?P<dbpass>.*)\'\);',line)
+                if password:
+                    dbinfo['dbpass'] = password.group('dbpass')
+                    #print (dbinfo['dbpass'])
+                host = re.search('define\(\'DB_HOST\'\,\ ?\'(?P<dbhost>.*)\'\);',line)
+                if host:
+                    dbinfo['dbhost'] = host.group('dbhost')
+                    #print(dbinfo['dbhost'])
+        with open(myconf, 'w')as conf:
+                print("[client]",file=conf)
+                print("{}{}".format("host=",dbinfo['dbhost']),file=conf)
+                print("{}{}".format("user=",dbinfo['dbuser']),file=conf)
+                print("{}{}".format("password=",dbinfo['dbpass']),file=conf)
+        args = "mysqldump --defaults-extra-file=" + myconf + " " + dbinfo['dbname'] + " > " + target
+        subprocess.run(args, shell=True)
+        os.remove(myconf)
 
-#archive_files(source)
-#prune_archives(destination,maxarchives)
-identify_wp_installs(source)
+archive_files(source)
+prune_archives(destination,maxarchives)
