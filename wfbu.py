9cd1e4f8c28eceb312e142a3b129393e56680b95
Kyle Stewart <git@kylestewart.com>
2018-11-13 12:30:51 -0600

    Empowering the script, take 2

--- wfbu.py
+++ wfbu.py
@@ -1,101 +1,115 @@
 import os
 import shutil
 import datetime
-import json
 import re
 import subprocess
 import boto3 #AWS Remote, installed via pip
 
 class backup:
+    #def __init__():
 
 #Create datestamped archive directory at destination
-    def create_archive_dir(destination):
+    def create_archive_dir(self,destination):
         date = datetime.datetime.now().strftime('%Y-%m-%d_%H%M')
-        archive = os.path.join(self.destination,date)
+        archive = os.path.join(destination,date)
         try:
             os.makedirs(archive)
         except:
-            archive = str(archive) + "." + str(datetime.datetime.now().strftime('%f')) 
+            archive = str(archive) + "." + str(datetime.datetime.now().strftime('%f'))
             os.makedirs(archive)
         return(archive)
 
 #Get a list of archives and sort the list by creation time
-    def list_archives(self):
+    def list_archives(self,destination):
         archives = []
-        for dir in os.listdir(self.destination):
-            archives.append(os.path.join(self.destination,dir))
+        for dir in os.listdir(destination):
+            archives.append(os.path.join(destination,dir))
         archives = sorted(archives, key=os.path.getctime)
         return(archives)
 
 #Return the name of the newest archive directory
-    def get_newest_archive(self):
-        archives = self.list_archives()
+    def get_newest_archive(self,destination):
+        archives = self.list_archives(destination)
         newest_index = len(archives) - 1
         newest_archive = archives[newest_index]
         return (newest_archive)
 
 #Prune old backup directories, as defined in the settings file
-    def prune_archives(self):
-        archives = self.list_archives()
-        if len(archives) > self.maxarchives:
-            toPrune = int(len(archives)) - int(self.maxarchives)
+    def prune_archives(self,destination,maxarchives):
+        archives = self.list_archives(destination)
+
+        if len(archives) > maxarchives:
+            toPrune = int(len(archives)) - maxarchives
             archives = sorted(archives, key=os.path.getctime)
+
             i = 0
             while i < toPrune:
-                shutil.rmtree(archives[i])
+
+                #Check if the object is a file.  If it is, leave it alone.
+                if os.path.isfile(os.path.join(destination,archives[i])):
+                    pass
+                else:
+                    shutil.rmtree(archives[i])
                 i += 1
+
         else:
             toPrune = 0
+
         return("{} {} {}".format("Pruned",toPrune,"Archives"))
 
 #Search source directories for WordPress Config files
-    def identify_wp_installs(self):
+    def identify_wp_installs(self,source):
         wpInstalls = []
-        for dir in os.listdir(self.source):
-            for file in os.listdir(os.path.join(self.source,dir)):
+        for dir in os.listdir(source):
+            for file in os.listdir(os.path.join(source,dir)):
                 if file == "wp-config.php":
                     wpInstalls.append(dir)
         return(wpInstalls)
 
 #Backup content of source directories
-    def archive_files(self):
-        dstPath = self.create_archive_dir()
-        for object in os.listdir(self.source):
-            if self.compression:
-                if os.path.isfile(os.path.join(self.source,object)):
+    def archive_files(self,source,destination,compression,archive_dir=None):
+
+        #Use the provided Archive Directory, or make one if it isn't provided
+        if archive_dir == None:
+            dstPath = self.create_archive_dir(destination)
+        else:
+            dstPath = archive_dir
+
+        for object in os.listdir(source):
+            if compression:
+                if os.path.isfile(os.path.join(source,object)):
                     zipfile = os.path.join(dstPath,object)
-                    shutil.make_archive(zipfile,'zip',dstPath,os.path.join(self.source,object))
-                if os.path.isdir(os.path.join(self.source,object)):
+                    shutil.make_archive(zipfile,'zip',dstPath,os.path.join(source,object))
+                if os.path.isdir(os.path.join(source,object)):
                     zipdir = os.path.join(dstPath,object)
                     os.makedirs(zipdir)
-                    shutil.make_archive(os.path.join(zipdir,object),'zip',zipdir,os.path.join(self.source,object))
+                    shutil.make_archive(os.path.join(zipdir,object),'zip',zipdir,os.path.join(source,object))
             else:
-                if os.path.isfile(os.path.join(self.source,object)):
-                    shutil.copyfile(os.path.join(self.source,object),os.path.join(dstPath,object))
-                if os.path.isdir(os.path.join(self.source,object)):
-                    shutil.copytree(os.path.join(self.source,object),os.path.join(dstPath,object))
-        
-        if self.backup_wp_dbs:
-            self.backup_wp_databases()
-        
-        if self.autoprune:
-            self.prune_archives()
-        
-        if self.remote:
-            self.remote_upload()
+                if os.path.isfile(os.path.join(source,object)):
+                    shutil.copyfile(os.path.join(source,object),os.path.join(dstPath,object))
+                if os.path.isdir(os.path.join(source,object)):
+                    shutil.copytree(os.path.join(source,object),os.path.join(dstPath,object))
+
         return("Archiving Complete")
 
 
 #Take a list of WP Installs, snag the DB credentials, and take the dumps.  The newest created archive dir will get the dump files.
-    def backup_wp_databases(self):
-        wpInstalls = self.identify_wp_installs()
+    def backup_wp_databases(self,source,destination,archive_dir=None):
+        wpInstalls = self.identify_wp_installs(source)
         for install in wpInstalls:
             dbinfo  = {}
-            srcdir  = os.path.join(self.source,install)
-            dstPath = os.path.join(self.destination,self.get_newest_archive())
+            srcdir  = os.path.join(source,install)
+
+            #Use the provided Archive Directory, or use the newest one if it isn't provided
+            if archive_dir == None:
+                dstPath = os.path.join(destination,self.get_newest_archive(destination))
+            else:
+                dstPath = archive_dir
+
             dstdir  = os.path.join(dstPath,install)
             myconf  = os.path.join(dstdir,"mylogin.cnf")
             target  = os.path.join(dstdir,install) + ".sql"
+
             with open(os.path.join(srcdir,'wp-config.php')) as config_file:
                 for line in config_file:
                     name = re.search('define\(\'DB_NAME\'\,\ ?\'(?P<dbname>.*)\'\);',line)
@@ -110,25 +124,29 @@ class backup:
                     host = re.search('define\(\'DB_HOST\'\,\ ?\'(?P<dbhost>.*)\'\);',line)
                     if host:
                         dbinfo['dbhost'] = host.group('dbhost')
+
             with open(myconf, 'w')as conf:
                 print("[client]",file=conf)
                 print("{}{}".format("host=",dbinfo['dbhost']),file=conf)
                 print("{}{}".format("user=",dbinfo['dbuser']),file=conf)
                 print("{}{}".format("password=",dbinfo['dbpass']),file=conf)
+
             args = "mysqldump --defaults-extra-file=" + myconf + " " + dbinfo['dbname'] + " > " + target
             subprocess.run(args, shell=True)
             os.remove(myconf)
+
         return("Database backup complete")
 
 
 #Use boto3 to create a session with S3
-    def remote_connect(self):
+    def remote_connect(self,access_key,secret_key):
         session = boto3.Session(
-            aws_access_key_id=self.access_key,
-            aws_secret_access_key=self.secret_key,
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
         )
         return(session)
 
+#This works but not useful at the moment.  After connecting, list the buckets.
     def remote_ls_buckets(self):
         buckets = []
         session = self.connect()
@@ -138,9 +156,9 @@ class backup:
         return(buckets)
 
 #Until boto3 supports directory syncing, we'll use this method to shell out and use the aws CLI.
-    def remote_upload(self):
-        src = self.get_newest_archive()
-        dest = "s3://" + self.bucket_name + self.remote_folder
+    def remote_upload(self,destination,bucket_name,remote_folder):
+        src = self.get_newest_archive(destination)
+        dest = "s3://" + bucket_name + remote_folder
         cmd = "{} {} {} {} {} {} {}".format("aws","s3","sync",src,dest,"--size-only","--delete")
         subprocess.run(cmd,shell = True)
         return("Upload Complete")
